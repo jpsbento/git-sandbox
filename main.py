9c96b04b17fa8a743504b7e9e5570d897db4cb68
Joao Bento <jpsbento@gmail.com>
2024-07-02 10:53:25 +0100

    Initial version of the main function

---
+++ main.py
@@ -0,0 +1,10 @@
+def total_price(price, units):
+    return price * units
+
+def main():
+    price = 100
+    units = 3
+    print("The total price is %s" % total_price(price, units))
+
+if __name__ == "__main__":
+    main()

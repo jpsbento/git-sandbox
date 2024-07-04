72fa33db011bd9330fe860d4b479481dc9926563
Joao Bento <jpsbento@gmail.com>
2024-07-04 15:07:24 +0100

    adding the word created!

--- main.py
+++ main.py
@@ -7,5 +7,5 @@ def main():
     print("The total price is %s" % total_price(price, units))
 
 if __name__ == "__main__":
-    print('Running the best program ever')
+    print('Running the best program ever created!')
     main()

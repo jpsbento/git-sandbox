6c5de44a969948c4b1b781ed4642f785ce0cd7b8
Joao Bento <jpsbento@gmail.com>
2024-07-05 11:11:01 +0100

    Changing the name of the total function and the variable names are better now.

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-def tot(pr, u):
+def total(pr, u):
     return pr * u
 
 def mfunc():
     price=200
     units=5
-    tot = tot(price, units)
-    print("The total price is ", tot, " pounds")
+    total_price = total(price, units)
+    print("The total price is ", total_price, " pounds")
 
 mfunc()

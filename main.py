ff246590912b0fed32b3e4a824f3886b07ad42b9
Anna Durrant <annardurrant@gmail.com>
2024-07-05 11:03:23 +0100

    Added pounds to euros conversion

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from utils import *
 
 def mfunc(price=200,units=5):
     tot = tot(price, units)
+    tot_eur = tot(price*1.2,units)
     print("The total price is ", tot, " pounds")
+    print("And that is equivalent to ", tot_eur, " euros")
 
 mfunc()

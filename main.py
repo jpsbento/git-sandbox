460a6ee3f77a97814b83f0a2e08a805284f5f67a
Nell Grabham <nellgrab@icloud.com>
2024-07-05 11:04:25 +0100

    Nells changes to make readability of code better 1

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+#define a function that takes two arguments, price and units
+#the function should return the total price
 def tot(pr, u):
     return pr * u
 
+# define a function that calls the tot function
+# the function should print the total price
 def mfunc():
     price=200
     units=5
@@ -8,3 +12,12 @@ def mfunc():
     print("The total price is ", tot, " pounds")
 
 mfunc()
+
+#now will define a second function calling the same tot function to define a new total price in dollars 
+def mfunc2():
+    price=300
+    units=6
+    tot = tot(price, units)
+    print("The total price is ", tot, "dollars")
+
+mfunc2()

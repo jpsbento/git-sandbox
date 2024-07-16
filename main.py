64ed1dd8d3ce1bf1f66bacc456c38732da73e680
Bradley March <bradley.march@hotmail.com>
2024-07-16 11:48:19 +0100

    Updated function to allow for different price/unit values

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 def total_price(price, units):
     return price * units
 
-def main():
-    price = 100
-    units = 3
+def main(price=100 ,units=3):
     print("The total price is %s" % total_price(price, units))
 
 if __name__ == "__main__":
